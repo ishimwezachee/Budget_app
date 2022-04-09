23e553a74495b9c6260bbe7d7fb723a8233cd8c9
ishimwezachee <zacheeishimwe@gmail.com>
2022-04-09 10:29:24 +0200

    Add right path

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,7 +1,7 @@
 module CategoriesHelper
   def example_img(category)
-    img_example = '/assets/images/default.png'
-    if category.icon.present? and category.icon.last(4) == '.png'
+    img_example = 'https://cdn0.iconfinder.com/data/icons/simple-seo-and-internet-icons/512/budget_planning-512.png'
+    if category.icon.present? and category.icon.last(4) == '.png' 
       category.icon
     else
       img_example
