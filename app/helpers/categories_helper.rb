a4597d46d59f647c78412ee32638deff0628c4c5
ishimwezachee <zacheeishimwe@gmail.com>
2022-04-09 10:16:18 +0200

    Add right path

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,6 +1,6 @@
 module CategoriesHelper
   def example_img(category)
-    img_example = '/assets/default.png'
+    img_example = '/assets/images/default.png'
     if category.icon.present? and category.icon.last(4) == '.png'
       category.icon
     else
